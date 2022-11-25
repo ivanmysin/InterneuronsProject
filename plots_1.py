ee13e7b2c87fa1751778e2b89c38cb65031d6a65
ivan <imysin@mail.ru>
2022-11-25 22:44:42 +0300

    basic files are created

---
+++ plots_1.py
@@ -0,0 +1,38 @@
+import plots_lib
+
+def plot_theta_phase_disrtibution():
+    pass
+    """
+    Создаем окно с множеством графиков.
+    пробегаемся по типам интренейронов и строим график для каждого типа
+    вызываем функцию plot_phases из plots_lib
+    """
+
+
+
+def plot_spike_rate_during_states():
+    """
+    Создаем окно с множеством графиков.
+    пробегаемся по типам интренейронов и строим график для каждого типа
+    вызываем функцию plot_bars из plots_lib
+    Сравниваем частоту разрядов в тета-, нетета состояниях и рипплах
+    """
+
+
+
+def plot_phases_gamma_theta_vs_nontheta():
+    """
+    Создаем окно с множеством графиков.
+    пробегаемся по типам интренейронов и строим график для каждого типа
+    вызываем функцию plot_pairs из plots_lib
+    Сравниваем фазу разрядов относительно медленного гамма в тета- и нетета состояниях
+    """
+
+
+
+
+
+if __name__ == "__main__":
+    plot_theta_phase_disrtibution()
+    plot_spike_rate_during_states()
+    plot_phases_gamma_theta_vs_nontheta()
---
+++ processing_lib.py
@@ -0,0 +1,53 @@
+
+
+def get_ripples_episodes_indexes(lfp, fs):
+    """
+    :param lfp: сигнал lfp
+    :param fs: частота дискретизации
+    :return:  массив начал и концов риппл событий
+    """
+
+    pass
+
+def get_theta_non_theta_epoches(theta_lfp, delta_lfp):
+    """
+    :param theta_lfp: отфильтрованный в тета-диапазоне LFP
+    :param delta_lfp: отфильтрованный в дельа-диапазоне LFP
+    :return: массив индексов начала и конца тета-эпох, другой массив для нетета-эпох
+    """
+    pass
+
+def get_circular_mean_R(filtered_lfp, fs, spike_train):
+    """
+    :param filtered_lfp: отфильтрованный в нужном диапазоне LFP
+    :param fs: частота дискретизации
+    :param spike_train: времена импульсов
+    :return: циркулярное среднее и R
+    """
+
+def get_mean_spike_rate_by_epoches(spike_train, epoches):
+    """
+    :param epoches: массив начал и концов эпох
+    :param spike_train: времена импульсов
+    :return: циркулярное среднее и R
+    """
+    pass
+
+
+class InterneuronClassifier:
+
+    def __int__(self, path2data):
+        """
+        :param path2data: Путь к файлу с данными, покоторым будем классифицировать
+        :return:
+        """
+
+    def transpose(self, X):
+        """
+        :param X: вектор - описание потока импульсов
+        :return: class name
+        """
+
+
+
+
