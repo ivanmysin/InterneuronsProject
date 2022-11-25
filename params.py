68f804fe40e38a281c8b6d090bc22a11dbcd683c
ivan <imysin@mail.ru>
2022-11-25 21:47:16 +0300

    Fist commit

---
+++ params.py
@@ -0,0 +1,10 @@
+
+
+rhythms_freqs_range = {
+    "delta" : [1, 4],
+    "theta" : [4, 12],
+    "slow_gamma" : [25, 45],
+    "middle_gamma" : [45, 80],
+    "fast_gamma" : [80, 150],
+    "ripples"    : [120, 250],
+}
