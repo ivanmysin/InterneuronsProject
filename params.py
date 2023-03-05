e65a56f09ded1899e1755eb1b154c6f351a6c168
ivanmysin <imysin@mail.ru>
2023-03-05 16:42:08 +0300

    multiprocessing is added in preprocessing_1

--- params.py
+++ params.py
@@ -31,5 +31,5 @@ feasures_names.extend(["ripples_phi", "ripples_R"])
 
 # spike rate in theta, non-theta state and ripples
 feasures_names.extend(["ts_spike_rate", "ts_spike_rate_std"])
-feasures_names.extend(["non_ts_mean_spike_rate", "non_ts_std_spike_rate"])
+feasures_names.extend(["non_ts_spike_rate", "non_ts_std_spike_rate"])
 feasures_names.extend(["ripples_spike_rate", "ripples_spike_rate_std"])
