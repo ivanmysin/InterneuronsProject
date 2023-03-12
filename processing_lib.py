2be80b7fd04e56db2f9a67d64fea3fec90abd4dc
IgorPov <58955706+IgorPov@users.noreply.github.com>
2023-03-12 23:37:04 +0300

    Update processing_lib.py

--- processing_lib.py
+++ processing_lib.py
@@ -4,16 +4,57 @@ from scipy.signal.windows import parzen
 from numba import jit
 
 #@jit(nopython=True)
-def clear_articacts(lfp, win_size=101, threshold=0.1):
-    lfp = lfp - np.mean(lfp)
-    lfp_std = np.std(lfp)
-    is_large = np.logical_and( (lfp > 10*lfp_std), (lfp < 10*lfp_std) )
-    is_large = is_large.astype(np.float64)
-    is_large = np.convolve(is_large, parzen(win_size), mode='same')
-    is_large = is_large > threshold
-
-    lfp[is_large] = np.random.normal(0, 0.001*lfp_std, np.sum(is_large) )
-    return lfp
+def clear_articacts(lfp, fs = 1250, win_len = 2500, tol_thresh_mult = 15, min_len_mult = 3, show = False):
+    #win_len - длительнгость фрагментов, на которые нарезается сигнал
+    #tol_thresh_mult - чистка проводится по выбросам (fliers), но многие выбросы визуально близки
+    #к "нормальным" значениям размаха потенциала, из-за чего сигнал слишком мелко нарезается, если
+    #учитывать все выбросы. Какую-то долю выбросов можно перевести в категорию "нормальных" значений,
+    #что и регулируется этим параметром.
+    #min_len_mult - для того, чтобы убрать самые короткие фрагменты, например, если фрагмент 
+    #чистого сигнала меньше трех длительностей минимального фрагмента, то он отбрасывается.
+    mm_list = [] #список размаха значений (разница между минимумом и максимумом) фрагментов lfp
+    times = np.arange(len(lfp)) / 1250
+    for i in range(0, int(np.floor(len(lfp)/win_len))):
+        lfp_fragment = list(lfp[i * win_len : (i + 1) * win_len])
+        mm_list.append(abs(np.max(lfp_fragment) - np.min(lfp_fragment)) )
+    b = plt.boxplot(mm_list)
+    plt.close()
+    for i in b["fliers"]:
+        mm_fliers = i.get_data()[1]
+    fl_idxs = [mm_list.index(i) for i in mm_fliers]
+    
+    anti_fl_idxs = [i for i in range(len(mm_list)) if i not in fl_idxs]
+    tol_thresh = np.std(np.asarray(mm_list)[anti_fl_idxs]) * tol_thresh_mult #tolerance threshold
+    fl_tol_idxs = [i for i, j in enumerate(mm_list) if j >= tol_thresh]
+    
+    clear_borders_t, clear_borders_idxs, tmp_t, tmp_idxs = [], [], [], []        
+    for i in range(0, int(np.floor(len(lfp)/win_len))):
+        if i not in fl_tol_idxs:
+            t = list(times[i * win_len : (i + 1) * win_len]) 
+            tmp_t += [t[0], t[-1]]
+            tmp_idxs += [i * win_len, (i + 1) * win_len]           
+        if i in fl_tol_idxs or (i + 1) == int(np.floor(len(lfp)/win_len)):  
+            if len(tmp_t) > 0 and (np.max(tmp_idxs) - np.min(tmp_idxs)) >= (min_len_mult * win_len):
+                clear_borders_t.append([np.min(tmp_t), np.max(tmp_t)])
+                clear_borders_idxs.append([np.min(tmp_idxs), np.max(tmp_idxs)])
+            tmp_t, tmp_idxs = [], []
+    
+    if show == True:
+        plt.plot(times, lfp, color = 'red')
+        for chunk_idxs in clear_borders_idxs:
+            plt.plot(np.arange(chunk_idxs[0], chunk_idxs[1]) / fs, 
+                     list(lfp[chunk_idxs[0] : chunk_idxs[1]]), color = 'black')    
+        plt.show()
+    
+    lfp_sample = []
+    lfp_chunks = [list(lfp[ch_idxs[0] : ch_idxs[1]]) for ch_idxs in clear_borders_idxs]    
+    for i in lfp_chunks: lfp_sample += i
+    lfp_norm = (lfp - np.mean(lfp_sample)) / np.std(lfp_sample) #Нормализация!
+    if len(clear_borders_idxs) > 1: #Зануляем артефактные отрезки сигнала
+        cut_offs = [[clear_borders_idxs[i][1], clear_borders_idxs[i + 1][0]] for i in range(len(clear_borders_idxs) - 1)]
+        for cut_idxs in cut_offs:
+            lfp_norm[cut_idxs[0] : cut_idxs[1]] = 0    
+    return lfp_norm, clear_borders_idxs #clear_borders_t, fl_tol_idxs,
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
@@ -22,12 +63,11 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     b, a = butter(order, [low, high], btype='band')
     return b, a
 
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = filtfilt(b, a, data)
-    return y
-
+def fir_bandpass_filter(lfp_norm, fir_Order, band): #3000, theta_band [4, 11], delta_band [0.5, 3.5]
+    fir = signal.firwin(fir_Order, band, fs = 1250, pass_zero = False) #band pass
+    print("len(lfp_norm): ", len(lfp_norm))
+    lfp_filtered = signal.filtfilt(fir, 1, lfp_norm)
+    return lfp_filtered
 
 def merge_ripple_zones(starts, ends, fs, gap_to_unite=5):
     """
@@ -81,85 +121,111 @@ def get_ripples_episodes_indexes(ripples_lfp, fs, threshold=4, accept_win=0.02):
     return ripples_epoches
 
 @jit(nopython=True)
-def get_theta_non_theta_epoches(theta_lfp, delta_lfp, fs, theta_threshold=2, accept_win=2):
-    """
-    :param theta_lfp: отфильтрованный в тета-диапазоне LFP
-    :param delta_lfp: отфильтрованный в дельа-диапазоне LFP
-    :param theta_threshold : порог для отделения тета- от дельта-эпох
-    :param accept_win : порог во времени, в котором переход не считается.
-    :return: массив индексов начала и конца тета-эпох, другой массив для нетета-эпох
-    """
-    sampling_period = 1 / fs
-
-    theta_amplitude = np.abs(theta_lfp)
-    delta_amplitude = np.abs(delta_lfp)
-
-    relation = theta_amplitude / delta_amplitude
-
-    theta_state_inds = []
-    non_theta_state_inds = []
-
-    for ind, i in enumerate(relation):
-        if i >= theta_threshold:
-            theta_state_inds.append(ind)
+def get_theta_non_theta_epoches(theta_lfp, delta_lfp, lfp_norm, clear_borders_idxs, theta_thresh = 2, theta_min_dur_mult = 1, accept_win = 10, show = False):     
+    #По алгоритму из (Kocsis B et al 2022)
+    '''
+    clear_borders_idxs - граничные индексы "чистых" участков lfp без артефактов
+    theta_thresh - Пороговое значение отношения огибающих (тета к дельта) для выделения индексов участков с преобладанием тета-гармоник
+    theta_min_dur_mult - множитель, задающий минимальную длительность тета-эпохи (умножается на число отсчетов в секунду)
+    accept_win (лучше оставить равным 10) - для предварительного слияния очень близко расположенных тета-участков
+    '''
+    from copy import deepcopy as dc
+    def merging_rel(intervals, rel_thresh_mult):
+        #Слияние последующих интервалов с предыдущими при условии, что длительность разделяющих
+        #их отрезков меньше суммы половин длительностей предшествующего и последующего интервалов
+        #в rel_thresh_mult раз (субЪективный выбор)
+        intervals_corr, incr_ep = [], dc(intervals[0]) #incr_ep - приращиваемый интервал
+        for i in range(1, len(intervals)):
+            sub_len = (intervals[i][1] - intervals[i][0]) / 2 + (intervals[i - 1][1] - intervals[i - 1][0])
+            #sub_len = intervals[i][1] - intervals[i][0] #длительность данного тета-интрвала
+            inter_len = intervals[i][0] - intervals[i - 1][1] #время между текущим и предыдущим тета-интервалом
+            if sub_len >= inter_len * rel_thresh_mult:
+                incr_ep[1] = intervals[i][1]
+            else:
+                intervals_corr.append(incr_ep)
+                incr_ep = dc(intervals[i])
+        return intervals_corr
+    
+    def merging_abs(intervals, abs_thresh):
+        #Соединие интервалов при условии, что время между ними меньше выбранного порога        
+        intervals_corr, incr_ep = [], dc(intervals[0])
+        for i in range(1, len(intervals)):
+            inter_len = intervals[i][0] - intervals[i - 1][1]
+            if inter_len < abs_thresh:
+                incr_ep[1] = intervals[i][1]
+            else:
+                intervals_corr.append(incr_ep)
+                incr_ep = dc(intervals[i])
+        return intervals_corr
+    
+    #Получение мгновенных амплитуд тета и дельта составляющих:                      
+    sub_theta_env = np.abs(hilbert(theta_lfp))
+    sub_delta_env = np.abs(hilbert(delta_lfp))    
+    
+    #Нахождение соотношения мгновенных амплитуд тета и дельта составляющих: 
+    theta2delta = sub_theta_env / sub_delta_env
+    theta2delta[theta2delta > 10] = 10 #Замена зашкаливающих значений 
+    theta2delta = np.array([i if not np.isnan(i) else 0 for i in theta2delta])
+              
+    #Основной способ выделения первичных тета-эпох:
+    theta_idxs = [i for i, j in enumerate(theta2delta) if j >= theta_thresh and lfp_norm[i] != 0]
+    theta_epochs, borders = [], [theta_idxs[0]]
+    for i in range(1, len(theta_idxs) - 1):
+        if theta_idxs[i + 1] - theta_idxs[i] > accept_win:
+            borders.append(theta_idxs[i])
+            if len(borders) == 2:
+                if borders[1] - borders[0] > 0:
+                    theta_epochs.append(borders)
+                borders = [theta_idxs[i + 1]]  
+    '''
+    #Дополнительный способ выделения границ тета-событий (для контроля, можно отключить):
+    theta_idxs_control = [i if j >= theta_thresh and lfp_norm[i] != 0 else np.nan 
+                          for i, j in enumerate(theta2delta)]
+    theta_epochs_control, theta_subset = [], []
+    for i in theta_idxs_control:
+        if not np.isnan(i): 
+            theta_subset.append(i)
         else:
-            non_theta_state_inds.append(ind)
-
-    theta_state_inds = np.asarray(theta_state_inds)
-    theta_array = theta_state_inds[1:] - theta_state_inds[:-1]
-
-    non_theta_state_inds = np.asarray(non_theta_state_inds)
-    non_theta_array = non_theta_state_inds[1:] - non_theta_state_inds[:-1]
-
-    theta_states = []
-    start_theta = 0
-
-    for i in range(len(theta_array)):
-        if theta_array[i] != 1:
-            stop_theta_ind = theta_state_inds[i]
-
-            epoch = [theta_state_inds[start_theta], stop_theta_ind]
-
-            theta_states.append(epoch)
-
-            start_theta = i + 1
-
-    time_filtered_theta_states = []
-
-    for state in theta_states:
-
-        length = state[1] - state[0]
-        secs = length / sampling_period
-
-        if secs >= accept_win:
-            time_filtered_theta_states.append(state)
-
-    non_theta_states = []
-    start_non_theta = 0
-
-    for i in range(len(non_theta_array)):
-        if non_theta_array[i] != 1:
-            stop_non_theta = non_theta_state_inds[i]
-
-            epoch = [non_theta_state_inds[start_non_theta], stop_non_theta]
-
-            non_theta_states.append(epoch)
-
-            start_non_theta = i + 1
-
-    time_filtered_non_theta_states = []
-
-    for state in non_theta_states:
-
-        length = state[1] - state[0]
-        secs = length / sampling_period
-
-        if secs >= accept_win:
-            time_filtered_non_theta_states.append(state)
-
-    theta_epoches = np.asarray(time_filtered_theta_states)
-    non_theta_epoches = np.asarray(time_filtered_non_theta_states)
-    return theta_epoches, non_theta_epoches
+            if len(theta_subset) > 1:
+                theta_epochs_control.append([np.min(theta_subset), np.max(theta_subset)])
+                theta_subset = []
+    '''
+    theta_epochs_corr = merging_rel(theta_epochs, 2)
+    theta_epochs_corr = [i for i in theta_epochs_corr if i[1] - i[0] >= theta_min_dur_mult * 1250]
+    theta_epochs_corr = merging_abs(theta_epochs_corr, 1250)
+    #Получение границ дельта-эпох за счет инвертирования тета-интервалов: 
+    delta_epochs = ([[0, theta_epochs_corr[0][0]]]
+                    + [[theta_epochs_corr[i][1], theta_epochs_corr[i + 1][0]] for i in range(len(theta_epochs_corr) - 1)]) 
+                    #+ [[theta_epochs_corr[-1][1], len(lfp_norm) - 1]])
+    a = np.transpose(delta_epochs)[0] #Вырезание артефактных интервалов
+    false_epochs_nums = [len(delta_epochs) - len(a[a >= i]) - 1 for i in np.transpose(clear_borders_idxs)[0]]
+    delta_epochs_corr = [delta_epochs[i] for i in range(len(delta_epochs)) if i not in false_epochs_nums] 
+    
+    if show == True: #Визуальная проверка (задать свой адрес сохранения):              
+        ix = 10000 #Для удобной развертки на одну картинку по 10000 отсчетов
+        def get_idx_list(epoch_idxs):
+            idxs = [np.nan for k in range(0, epoch_idxs[0][0])]
+            for j in range(len(epoch_idxs)):
+                idxs += [1 for k in range(epoch_idxs[j][0], epoch_idxs[j][1] + 1)]
+                if j < len(epoch_idxs) - 1:
+                    idxs += [np.nan for k in range(epoch_idxs[j][1] + 1, epoch_idxs[j + 1][0])]    
+            return np.array(idxs)       
+        #theta_uncorr_1 = [1 if not np.isnan(i) else i for i in theta_idxs_control] #исходные тета-эпохи
+        theta_uncorr_2 = get_idx_list(theta_epochs) 
+        #theta_control = get_idx_list(theta_epochs_control) * 2      
+        theta_corr = get_idx_list(theta_epochs_corr) * 3
+        delta_corr = get_idx_list(delta_epochs_corr) * 4
+        for i in range(1, int(np.floor(1999359 / 10000))):
+            fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (30, 10))                                        
+            ax.plot(np.arange(len(lfp_norm))[(i - 1) * ix : i * ix] / 1250, theta_uncorr_2[(i - 1) * ix : i * ix], color = 'blue')             
+            #ax.plot(np.arange(len(lfp_norm))[(i - 1) * ix : i * ix] / 1250, theta_control[(i - 1) * ix : i * ix], color = 'red')
+            ax.plot(np.arange(len(theta_corr))[(i - 1) * ix : i * ix] / 1250, theta_corr[(i - 1) * ix : i * ix], color = 'magenta')        
+            ax.plot(np.arange(len(delta_corr))[(i - 1) * ix : i * ix] / 1250, delta_corr[(i - 1) * ix : i * ix], color = 'green')
+            ax.plot(np.arange(len(theta2delta))[(i - 1) * ix : i * ix] / 1250, theta2delta[(i - 1) * ix : i * ix], color = 'black')         
+            fig.savefig("D:\\A_PopModels/test/" + 'sub_' + str(int((i - 1) * ix / 1250)) 
+                        + '-' + str(int(i * ix / 1250)) + '_.png')
+            
+    return theta_epochs_corr, delta_epochs_corr
 
 
 @jit(nopython=True)
